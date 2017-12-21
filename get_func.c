1bb5732e15c26c7549986ea1504cb5a3c5d3a27c
imperfectskillz <j.choi.89@gmail.com>
2017-12-21 09:11:51 +0000

    get_func completed

---
+++ get_func.c
@@ -0,0 +1,35 @@
+#include "monty.h"
+
+/**
+ *get_func - runs selected function
+ *@token: tokenized operation to run
+ *@stack: double pointer
+ *@line_number: line number of monty file
+ */
+
+void get_func(char *token, stack_t **stack, unsigned int line_number)
+{
+	int i = 0;
+
+	instruction_t func[] = {
+		{"push", push_stack},
+		{"pall", pall_stack},
+		{"pint", pint_stack},
+		{"pop", pop_stack},
+		{"swap", swap_stack},
+		{"add", add_stack},
+		{"nop", nop_stack},
+		{NULL, NULL}
+	};
+
+	while (func[i].opcode != NULL)
+	{
+		if (strcmp(token, func[i].opcode) == 0)
+		{
+			func[i].f(stack, line_number);
+		}
+	}
+
+	printf("L%d: unknown instruction %s\n", line_number, token);
+	exit(EXIT_FAILURE);
+}
