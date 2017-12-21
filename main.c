5e0e1051112f17746d630f9ceaeea24de4f34a20
imperfectskillz <j.choi.89@gmail.com>
2017-12-21 08:33:40 +0000

    added getline function

--- main.c
+++ main.c
@@ -5,12 +5,17 @@
  * then executes
  *@argc: number of arguments
  *@argv: arguments
- *Return: 0
+ *Return: nothing
  */
 
 int main(int argc, char *argv[])
 {
 	FILE *fp;
+	char *file_line = NULL;
+	char *token = NULL;
+	size_t size = 0;
+	unsigned int line_count = 1;
+	stack_t *stack = NULL;
 
 	if (argc != 2)
 	{
@@ -24,4 +29,15 @@ int main(int argc, char *argv[])
 		printf("Error: Can't open file %s", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-
+	while (getline(&file_line, &size, fp) != -1)
+	{
+		token = strtok(file_line, "\t \n \r");
+		if (token != NULL)
+		{
+			op_func(token, &stack, file_line);
+			line_count++;
+		}
+	}
+	fclose(fp);
+	exit(EXIT_SUCCESS);
+}
