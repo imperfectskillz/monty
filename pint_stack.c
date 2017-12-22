75d0ff35abf0dc8855af3ce9b09e582b13ee4584
imperfectskillz <j.choi.89@gmail.com>
2017-12-22 01:55:48 +0000

    added pint

---
+++ pint_stack.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ *pint_stack - prints value at top
+ *@stack: double pointer head
+ *@line_number: line numb
+ *Return: none
+ */
+
+void pint_stack(stack_t **stack, unsigned int line_number)
+{
+	printf("%d\n", (*stack)->n);
+}
